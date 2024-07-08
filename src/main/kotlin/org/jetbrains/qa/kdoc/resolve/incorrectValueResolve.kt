c52ee7c2f70ad0b1c094c4c3ed49810d836edcab
Andrei Tyrin <andrei.tyrin@jetbrains.com>
2024-07-08 15:27:51 +0200

    add case for false positive resolution

---
+++ src/main/kotlin/org/jetbrains/qa/kdoc/resolve/incorrectValueResolve.kt
@@ -0,0 +1,11 @@
+package org.jetbrains.qa.kdoc.resolve
+
+
+/**
+ * Incorrect resolve of [value] for K2
+ *
+ * // https://github.com/Kotlin/dokka/issues/3681
+ */
+fun useValue() {
+//    println(value)
+}
