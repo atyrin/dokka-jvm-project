2db0abf9c8855207ccdeb967a39ffc02ab391064
Andrei Tyrin <andrei.tyrin@jetbrains.com>
2026-07-22 16:34:53 +0200

    Refactor `LinksFieldRendering` to demonstrate structured examples of Javadoc link tags (`@link`, `@linkplain`, `@see`) and improve documentation clarity.

--- src/main/java/org/jetbrains/qa/java/javadoc/LinksFieldRendering.java
+++ src/main/java/org/jetbrains/qa/java/javadoc/LinksFieldRendering.java
@@ -1,37 +1,55 @@
 package org.jetbrains.qa.java.javadoc;
 
 /**
- *
- * Test link: {@link org.jetbrains.qa.kdoc.rendering.markdown}. End of link.
- * Test link: {@link org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks}. End of link.
- *
- * @see org.jetbrains.qa.kdoc.rendering.markdown
- * @see org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks
+ * Examples of Javadoc links grouped by tag.
  */
 public class LinksFieldRendering {
 
     /**
-     *
-     * Test link: {@link org.jetbrains.qa.kdoc.rendering.markdown}. End of link.
+     * {@link} supports package, class, member, and explicitly labelled links.
+     * <p>
+     * Package: {@link org.jetbrains.qa.kdoc.rendering.markdown}.
+     * <p>
+     * Class: {@link org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks}.
+     * <p>
+     * Label: {@link org.jetbrains.qa.kdoc.rendering.markdown package link}.
+     * <p>
+     * Class label: {@link org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks code blocks}.
+     * <p>
+     * Member anchor and label: {@link org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks#plain() plain code block}.
      */
-    void package1(){}
+    public void link() {
+    }
 
     /**
-     * declaration with seeAlso
+     * {@linkplain} supports package, class, member, and explicitly labelled links.
+     * <p>
+     * Package: {@linkplain org.jetbrains.qa.kdoc.rendering.markdown}.
+     * <p>
+     * Class: {@linkplain org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks}.
+     * <p>
+     * Label: {@linkplain org.jetbrains.qa.kdoc.rendering.markdown package link}.
+     * <p>
+     * Class label: {@linkplain org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks code blocks}.
+     * <p>
+     * Member anchor and label: {@linkplain org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks#plain() plain code block}.
+     */
+    public void linkPlain() {
+    }
+
+    /**
+     * The {@code @see} tag supports package, class, member, and explicitly labelled links.
+     *
      * @see org.jetbrains.qa.kdoc.rendering.markdown
-     */
-    void package2(){}
-
-
-    /**
-     *
-     * Test link: {@link org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks}. End of link.
-     */
-    void class1(){}
-
-    /**
-     * declaration with seeAlso
      * @see org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks
+     * @see org.jetbrains.qa.kdoc.rendering.markdown package link
      */
-    void class2(){}
+    public void see() {
+    }
+
+    /**
+     * @see org.jetbrains.qa.kdoc.rendering.markdown.CodeBlocks#plain() plain code block
+     */
+    public void seeWithAnchor() {
+    }
 }
